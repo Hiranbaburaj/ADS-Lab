2734e9c12a2cff9f34e5880ce819340112961319
Hiranbaburaj <116332096+Hiranbaburaj@users.noreply.github.com>
2022-11-02 22:28:50 +0530

    Create arrsort.c

---
+++ arrsort.c
@@ -0,0 +1,30 @@
+//sorting an array
+
+#include <stdio.h>
+int arr[100],value,i,j,n,temp;
+
+int main(){
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("Enter array element %d: ",i);
+        scanf("%d",&value);
+        arr[i]=value;  
+    }
+
+    for(i=n;i>0;i--){
+        for(j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){	
+			temp=arr[j];
+			arr[j]=arr[j+1];
+			arr[j+1]=temp;
+			}
+        }
+    }
+    printf("The array elemnts are: ");
+
+    for(i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
