2c3fa2e1709fd825e8f80ba5324aad16cdfcc119
Hiranbaburaj <116332096+Hiranbaburaj@users.noreply.github.com>
2022-11-11 09:38:04 +0530

    Add files via upload

---
+++ llq.c
@@ -0,0 +1,96 @@
+//queue using linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int value;
+    struct node *next;
+};
+
+typedef struct node node;
+node *head;
+
+void enqueue(){
+    node *temp, *t2;
+    int data;
+    printf("Enter the data you want to enter at the end: ");
+    scanf("%d",&data);
+    temp=(node*)malloc(sizeof(node));
+    temp->value=data;
+
+    if(head == NULL){
+        head=temp;
+        head->next=NULL;
+    }
+
+    else{
+        t2=head;
+        while(t2->next!=NULL){
+            t2=t2->next;
+        }
+        t2->next=temp;
+        temp->next=NULL;
+    }
+}
+
+void dequeue(){
+    if(head==NULL){
+        printf("Linked List is empty");
+    }
+    else if(head->next==NULL){
+        printf("%d is deleted",head->value); 
+        head=NULL;       
+    }      
+    else{
+    node *temp;
+    printf("%d is deleted",head->value);
+    temp=head->next;
+    head=temp;
+    }
+}
+
+void display(){
+    node*temp=head;
+    while(temp!=NULL){
+        printf("%d ",temp->value);
+        temp=temp->next;
+    }
+}
+
+void peek(){
+	printf("%d",head->value);
+}
+
+int main(){
+    int choice;
+    do{
+        printf("\nEnter the Choice\n 1.Enqueue\n 2.Dequeue\n ");
+        printf("3.Display the front element\n 4.Display the entire queue");
+        printf("\n 0.Exit\n");
+        scanf("%d",&choice);
+        switch(choice){
+            		case 1:
+				enqueue();
+				break;
+			case 2:
+				dequeue();
+				break;
+			case 3:
+				peek();
+				break;
+			case 4:
+				display();
+				break;
+                              
+                        case 0:
+				printf("Exit");
+				break;
+			deault:
+				printf("Invalid option ! Try Again..");
+        }
+    }
+    while(choice!=0);
+    return 0;
+
+}
